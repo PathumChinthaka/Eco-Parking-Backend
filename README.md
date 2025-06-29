8c8e74e81935a82a956781f8291b9667541b419a
Pathum Chinthaka <pathum.chinthaka@intellimorph.ai>
2025-06-29 10:34:54 +0530

    fix collection json not showing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ![Eureka Dashboard](./docs/screenshots/eureka_dashboard.png)
+<!-- Postman Api Collection -->
+[Postman Collection](./postman_collection.json)
--- user-service/src/main/java/com/eco_parking/user_service/controller/UserController.java
+++ user-service/src/main/java/com/eco_parking/user_service/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/user")
 @CrossOrigin(origins = {"*"})
 public class UserController {
 
